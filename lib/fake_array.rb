878d4d47c5d384f51e2f6a95f5206a92c455e102
Lia Skalkos <hello@liaskalkos.net>
2015-06-16 14:24:38 -0400

    All working

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,6 +1,26 @@
-class FakeArray
-
+require 'pry'
+class FakeArray < Array
+  attr_reader :entries
   # you'll need a splat in this class somewhere
+
+
+  def initialize(*entries)
+    @entries = entries
+  end
+
+  def [](index)
+    @entries[index]
+  end
+
+  def each
+    @entries.each{|entry| yield entry}
+  end
+
+  def first
+    @entries[0]
+  end
+
+
 
 
 
